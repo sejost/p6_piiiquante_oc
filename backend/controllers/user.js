253d2ba2c2b1ae64a1fe5b47a9dd633191f1ee4c
Sebastien Jost <jost.seb@gmail.com>
2022-06-23 09:29:44 +0200

    Adding user experiences signup and login functions

---
+++ backend/controllers/user.js
@@ -0,0 +1,46 @@
+const jwt = require('jsonwebtoken');
+const Str = require('@supercharge/strings'); //new package ' @supercharge/strings ' installed to generate a random string
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            user.save()
+                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error: 'test erreur' }));
+};
+
+const randomSecretToken = Str.random(32);
+
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: jwt.sign(
+                            { userId: user._id },
+                            //`${randomSecretToken}`,
+                            'RANDOM_TOKEN_SECRET',
+                            { expiresIn: '24h' }
+                        )
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+};
