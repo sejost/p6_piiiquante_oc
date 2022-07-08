e1c799929bc018f1a56ff6cb7089fbad726876c4
Sebastien Jost <jost.seb@gmail.com>
2022-07-08 12:49:01 +0200

    Add dotenv security

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const app = require('./app');
 
+require('dotenv').config();
+
 const normalizePort = val => {
     const port = parseInt(val, 10);
 
@@ -12,7 +14,7 @@ const normalizePort = val => {
     }
     return false;
 };
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT);
 app.set('port', port);
 
 const errorHandler = error => {
