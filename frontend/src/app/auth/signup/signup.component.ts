3f74f7ee5bc01dbd3e5e86bc902131a5e3602307
Sebastien Jost <jost.seb@gmail.com>
2022-07-08 10:31:41 +0200

    Fixing Frontend v74

--- frontend/src/app/auth/signup/signup.component.ts
+++ frontend/src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { catchError, EMPTY, switchMap, tap } from 'rxjs';
@@ -11,11 +11,11 @@ import { catchError, EMPTY, switchMap, tap } from 'rxjs';
 })
 export class SignupComponent implements OnInit {
 
-  signupForm!: UntypedFormGroup;
+  signupForm!: FormGroup;
   loading!: boolean;
   errorMsg!: string;
 
-  constructor(private formBuilder: UntypedFormBuilder,
+  constructor(private formBuilder: FormBuilder,
               private auth: AuthService,
               private router: Router) { }
 
